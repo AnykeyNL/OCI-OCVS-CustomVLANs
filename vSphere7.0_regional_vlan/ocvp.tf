704e9d0b448ce8c93626472e9b384d24b9c8d39b
Richard Garsthagen <the.anykey@gmail.com>
2021-08-03 16:20:06 +0200

    Support for Multi-AD Deployment

--- vSphere7.0_regional_vlan/ocvp.tf
+++ vSphere7.0_regional_vlan/ocvp.tf
@@ -13,7 +13,7 @@ resource oci_ocvp_sddc export_OCVS {
   nsx_edge_vtep_vlan_id        = oci_core_vlan.export_VLAN-OCVS-NSX-Edge-VTEP.id
   nsx_vtep_vlan_id             = oci_core_vlan.export_VLAN-OCVS-NSX-VTEP.id
   provisioning_subnet_id       = oci_core_subnet.export_Subnet-OCVS.id
-  ssh_authorized_keys          = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIQS8UDvLnTocbmm9q5ytQlO8MwE6BRhk3OVF/y2gfKbdQPGhKTisZCx8i0jJO8vPnp3a5ra0rg52LJ7UZ+FJNepbK//1Cb6iXPOP1nLft8FlvdC2YJOPrwYXAWL8ZQQY3W3qOID72vTwhns1ZJSAgSCn9gI8NUZCUwLxFMsQJwGtV+aPt2zsGnE1Vgzxsr795KgoRRt6E0UKT38BPxRkVXRp7JXwSjz8qLnqiAFp4JKFMCVud2pdyj0rQAMvb+QT49I4k9NKDQWzbjGRlsoUZDbc6lwnmo6jQ+pw3fZDS/CNi1+4udDJ6i5iaeU7hPnvQXfzSI3vOoFtrb83oZy2D phpseclib-generated-key"
+  ssh_authorized_keys          = var.ssh_public_key
   vmotion_vlan_id              = oci_core_vlan.export_VLAN-OCVS-vMotion.id
   vmware_software_version      = "7.0 update 2"
   vsan_vlan_id                 = oci_core_vlan.export_VLAN-OCVS-vSAN.id
--- vSphere7.0_regional_vlan/vars.tf
+++ vSphere7.0_regional_vlan/vars.tf
@@ -1,5 +1,5 @@
 ﻿# Example design for OCVS - vSphere 7.0
 variable region { default = "eu-frankfurt-1" }
 variable AD { default = "" }
-variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaj3bzcgi3kdg5ewytexdm652cyfhpgwzsf2sxub3dsj55tauwt6xa" }
-
+variable compartment_ocid { default = "" }
+variable ssh_public_key { default = "" }
