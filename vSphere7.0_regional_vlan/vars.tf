72f5e42ec05adb438bc8969bbd023804f27af2fd
Richard Garsthagen <the.anykey@gmail.com>
2021-08-03 16:17:19 +0200

    Support for Multi-AD Deployment

---
+++ vSphere7.0_regional_vlan/vars.tf
@@ -0,0 +1,5 @@
+﻿# Example design for OCVS - vSphere 7.0
+variable region { default = "eu-frankfurt-1" }
+variable AD { default = "" }
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaj3bzcgi3kdg5ewytexdm652cyfhpgwzsf2sxub3dsj55tauwt6xa" }
+
